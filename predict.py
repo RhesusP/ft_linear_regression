09d6a9a803b4cd2088d1c99004c1cca8f5638341
cbernot <camil.bernot.cb@gmail.com>
2024-11-23 18:58:37 +0100

    project ok. Need cleanup

---
+++ predict.py
@@ -0,0 +1,15 @@
+# Chargement des paramètres theta depuis le fichier
+file = open(".theta", "r")
+a = float(file.readline())  # theta_1
+b = float(file.readline())  # theta_0
+file.close()
+
+# Demander à l'utilisateur d'entrer le kilométrage
+mileage = input("Enter the mileage: ")
+mileage = float(mileage)
+
+# f(x) = a * x + b
+predicted_price = (a * mileage) + b
+
+# Affichage du prix prédit
+print(f"The predicted price for {mileage} km is: {predicted_price}")
--- trainer.py
+++ trainer.py
@@ -21,15 +21,18 @@ def gradient(x_matrix, y_matrix, theta_matrix):
     return 1 / m * x_matrix.T.dot(f_model(x_matrix, theta_matrix) - y_matrix)
 
 
-def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate, nb_iter):
-    history = np.zeros(nb_iter)
-    for i in range(0, nb_iter):
+def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate):
+    history = np.empty((0, 1))
+    # Gradient descent while minimizing the cost function
+    while True:
         theta_matrix = theta_matrix - learning_rate * gradient(x_matrix, y_matrix, theta_matrix)
-        history[i] = cost(x_matrix, y_matrix, theta_matrix)
+        history = np.vstack([history, [cost(x_matrix, y_matrix, theta_matrix)]])
+        if len(history) > 1 and history[-2] - history[-1] < 0.000001:
+            break
     return theta_matrix, history
 
 
-np.set_printoptions(suppress=True, precision=0)
+# np.set_printoptions(suppress=True, precision=0)
 file_path = "data.csv"
 
 x = np.empty((0, 1))
@@ -45,13 +48,13 @@ with open(file_path, "r") as file:
             y = np.vstack([y, [int(row[1])]])
 
 x_copy = x
-print("x: ", x)
+y_copy = y
 
 # Normalisation des données
-mean_x = np.mean(x)
-std_x = np.std(x)
-x = (x - mean_x) / std_x
-print("normalized x: ", x)
+x_min = np.min(x)
+x_max = np.max(x)
+x = (x - x_min) / (x_max - x_min)
+
 
 # So we have m and n and we search for params a and b in the linear model f(x) = ax + b
 # 1st step : F model
@@ -60,14 +63,13 @@ X = np.hstack((x, np.ones(x.shape)))  # Add a one column for bias
 
 # theta
 theta = np.ones((2, 1))
-theta_final, cost_history = descent_gradient(X, y, theta, learning_rate=0.1, nb_iter=50)
-print("theta final: ", theta_final)
+theta_final, cost_history = descent_gradient(X, y, theta, learning_rate=0.001)
 
 predictions = f_model(X, theta_final)
 figure, axis = plt.subplots(1, 2, figsize=(14, 7))
 
-axis[0].scatter(x_copy, y)
-axis[0].plot(x_copy, predictions, c='r')
+axis[0].scatter(x, y)
+axis[0].plot(x, predictions, c='r')
 axis[0].set_title("Linear regression")
 axis[0].set_xlabel("Mileage")
 axis[0].set_ylabel("Price")
@@ -79,3 +81,29 @@ axis[1].set_ylabel("Cost (MSE)")
 
 plt.tight_layout()
 plt.show()
+
+file = open(".theta", "w")
+# Remove the standardization before saving thetas
+theta_final[1] = theta_final[1] - theta_final[0] * x_min / (x_max - x_min)
+theta_final[0] = theta_final[0] / (x_max - x_min)
+
+print("theta final: ", theta_final)
+file.write(str(theta_final[0][0]) + "\n")
+file.write(str(theta_final[1][0]) + "\n")
+file.close()
+
+
+# file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
+
+
+# Coeficient de determination R^2
+# https://youtu.be/vG6tDQc86Rs?t=1251
+
+
+def r_squared(target, pred):
+    u = ((target - pred) ** 2).sum()  # residu de la somme des carrés
+    v = ((y - y.mean()) ** 2).sum()  # somme des carrés totale
+    return 1 - u / v
+
+
+print("R^2: ", r_squared(y, predictions))
