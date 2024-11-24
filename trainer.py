2edfe9e412505a5339fd4d59c32e93d5df85663b
cbernot <camil.bernot.cb@gmail.com>
2024-11-24 01:36:34 +0100

    Add graph animation

--- trainer.py
+++ trainer.py
@@ -21,15 +21,30 @@ def gradient(x_matrix, y_matrix, theta_matrix):
     return 1 / m * x_matrix.T.dot(f_model(x_matrix, theta_matrix) - y_matrix)
 
 
-def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate):
-    history = np.empty((0, 1))
+def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line, c_line):
+    cost_history = np.empty((0, 1))
+    iter = 0
     # Gradient descent while minimizing the cost function
     while True:
+        print('iter: ', iter)
         theta_matrix = theta_matrix - learning_rate * gradient(x_matrix, y_matrix, theta_matrix)
-        history = np.vstack([history, [cost(x_matrix, y_matrix, theta_matrix)]])
-        if len(history) > 1 and history[-2] - history[-1] < 0.000001:
+        if iter % 100 == 0:
+            pred_line.set_ydata(f_model(x_matrix, theta_matrix))
+            cost_value = cost(x_matrix, y_matrix, theta_matrix)
+            cost_history = np.vstack([cost_history, [cost_value]])
+            c_line.set_xdata(np.arange(len(cost_history)) * 100)
+            c_line.set_ydata(cost_history)
+
+            axis[1].relim()
+            axis[1].autoscale_view()
+
+            plt.pause(0.005)
+        # Stop the loop if the cost function converges
+        if len(cost_history) > 1 and cost_history[-2] - cost_history[-1] < 0.001:
             break
-    return theta_matrix, history
+        iter += 1
+    print("Number of iterations: ", iter)
+    return theta_matrix
 
 
 # np.set_printoptions(suppress=True, precision=0)
@@ -55,7 +70,6 @@ x_min = np.min(x)
 x_max = np.max(x)
 x = (x - x_min) / (x_max - x_min)
 
-
 # So we have m and n and we search for params a and b in the linear model f(x) = ax + b
 # 1st step : F model
 # X matrix
@@ -63,18 +77,17 @@ X = np.hstack((x, np.ones(x.shape)))  # Add a one column for bias
 
 # theta
 theta = np.ones((2, 1))
-theta_final, cost_history = descent_gradient(X, y, theta, learning_rate=0.001)
 
-predictions = f_model(X, theta_final)
+plt.ion()
 figure, axis = plt.subplots(1, 2, figsize=(14, 7))
 
 axis[0].scatter(x, y)
-axis[0].plot(x, predictions, c='r')
+prediction_line = axis[0].plot(x, f_model(X, theta), c='r')
 axis[0].set_title("Linear regression")
 axis[0].set_xlabel("Mileage")
 axis[0].set_ylabel("Price")
 
-axis[1].plot(range(len(cost_history)), cost_history)
+cost_line = axis[1].plot([], [], c='b')
 axis[1].set_title("Cost evolution")
 axis[1].set_xlabel("Number of iterations")
 axis[1].set_ylabel("Cost (MSE)")
@@ -82,18 +95,22 @@ axis[1].set_ylabel("Cost (MSE)")
 plt.tight_layout()
 plt.show()
 
+theta_final = descent_gradient(X, y, theta, 0.01, prediction_line[0], cost_line[0])
+
+predictions = f_model(X, theta_final)
+prediction_line[0].set_ydata(predictions)
+plt.pause(0.005)
+
 file = open(".theta", "w")
 # Remove the standardization before saving thetas
 theta_final[1] = theta_final[1] - theta_final[0] * x_min / (x_max - x_min)
 theta_final[0] = theta_final[0] / (x_max - x_min)
+file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
 
-print("theta final: ", theta_final)
-file.write(str(theta_final[0][0]) + "\n")
-file.write(str(theta_final[1][0]) + "\n")
 file.close()
 
-
-# file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
+plt.ioff()
+plt.show()
 
 
 # Coeficient de determination R^2
