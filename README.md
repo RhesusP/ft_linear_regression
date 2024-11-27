840c06e9222e2fd5114d35cd37bcdd1a4083e5e4
cbernot <camil.bernot.cb@gmail.com>
2024-11-27 14:00:24 +0100

    📝 Documentation

--- README.md
+++ README.md
@@ -32,12 +32,155 @@ Then, you can use the model to make predictions by running the following command
 python3 predict.py
 ```
 
-## About the project
+## The theory
 
 This project is an introduction to machine learning. The goal is to predict the price of a car based on its mileage. The
 model is trained using a linear regression algorithm.
 
 General steps are :
+
 1. Read the data from the `data.csv` file.
 2. Perform a linear regression on the data.
 3. Save $\theta_0$ and $\theta_1$ to a file.
+4. Use the model to make predictions.
+
+### The dataset
+
+The dataset is a CSV file containing two columns: `km` and `price`.
+
+We represent the dataset as follows : $(x, y)$, with
+
+- $x$ is the mileage of the car.
+- $y$ is the price of the car.
+
+From the dataset, we can deduce 2 important values:
+
+- $m$ : the number of samples (rows) in the dataset (here, $m = 24$).
+- $n$ : the number of features in the dataset (here, $n = 1$ = mileage).
+
+### The model
+
+The model is a linear regression model. It is represented by the following equation:
+$$
+f(x) = ax+ b
+$$
+The goal is to find the values of $a$ and $b$ that minimize the cost function $J(a, b)$.
+
+### The cost function
+
+The cost function measures errors between $a$ and $b$ and $y$ values in the dataset.
+An error is calculated using the Euclidean distance between the predicted value and the actual value as follows:
+$$
+error = f(x^(i)) - y^(i)
+$$
+
+The sum of all errors is calculated as follows:
+$$
+J(a, b) = \frac{1}{2m} \sum_{i=1}^{m} (f(x^(i)) - y^(i))^2
+$$
+
+This is known as the [Mean Squared Error (MSE)](https://en.wikipedia.org/wiki/Mean_squared_error).
+
+### Minimizing the cost function (Gradient Descent)
+
+The MSE is a sum of squared errors, so it have a convex shape with a single minimum. We are looking for this minimum with the [Gradient Descent algorithm](https://en.wikipedia.org/wiki/Gradient_descent).
+
+#### The Gradient Descent algorithm
+
+The Gradient Descent algorithm is as follows:
+
+1. Initialize $a$ and $b$ to random values (0 in this project).
+2. Calculate the derivative of this point.
+3. Move with a step size $\alpha$ in the opposite direction of the derivative.
+4. Repeat steps 2 and 3 until convergence.
+
+#### Learning rate
+
+The learning rate $\alpha$ is a hyperparameter that controls how much we are moving between iterations. This value is
+arbitrary and can be changed to improve the model. However, if the learning rate is too high, the model may not converge
+to the minimum. If the learning rate is too low, the model may take a long time to converge.
+
+#### Calculate a gradient
+
+For $a$ (or $\theta_1$) :
+$$
+\frac{\partial J}{\partial a} = \frac{1}{m} sum_{i=1}^{m} x (ax + b - y)
+$$
+
+For $b$ (or $\theta_0$) :
+$$
+\frac{\partial J}{\partial b} = \frac{1}{m} sum_{i=1}^{m} (ax + b - y)
+$$
+
+## The practical part
+
+We are going to perform a linear regression on the dataset using matrix operations. This will allow us to calculate the
+values of $a$ and $b$ in a single operation instead of iterating over the dataset.
+
+### The dataset
+
+We still have the same dataset $(x, y)$ as before.
+
+### The model (F)
+
+The model is represented by the following equation:
+$$
+F = X \theta
+$$
+
+Where:
+$\theta$ is a vector containing the values of $a$ and $b$ : $$\begin{pmatrix}a\\\ b\end{pmatrix}$$
+$X$ is a matrix containing the values of $x$ from the dataset and a column of 1s (for the bias) :
+$$
+\begin{pmatrix}x_1 & 1\\ x_2 & 1\\ \vdots & \vdots\\ x_m & 1\end{pmatrix}
+$$
+
+### The cost function (J)
+
+We have
+$$
+J(a, b) = \frac{1}{2m} \sum_{i=1}^{m} (a x^(i) + b - y^(i))^2
+$$
+
+Where :
+
+- $y^(i)$ is all the values of $y$ from the dataset.
+- $a x^(i) + b$ is $X \theta$.
+- the $\frac{1}{2m}$ and $^2$ are to simplify calculations.
+
+So, the matrix form of the cost function is:
+$$
+J(\theta) = \frac{1}{2m} \sum_{i=1}^{m} (X \theta - Y)^2
+$$
+
+### The gradient
+
+We have formulas for the two gradients:
+$$
+\frac{\partial J(a, b)}{\partial a} = \frac{1}{m} \sum_{i=1}^{m} x (a x + b - y)
+$$
+
+$$
+\frac{\partial J(a, b)}{\partial b} = \frac{1}{m} \sum_{i=1}^{m} (a x + b - y)
+$$
+
+We put these two gradients in a vector:
+
+$$
+\frac{\partial J(\theta)}{\partial \theta} = \begin{pmatrix}\frac{\partial J(a, b)}{\partial a}\\\ \frac{\partial J(a,
+b)}{\partial b}\end{pmatrix}
+$$
+
+This vector calculate all the J derivatives for each $\theta$.  
+We have can calculate the gradient with the following formula:
+$$
+\frac{\partial J(\theta)}{\partial \theta} = \frac{1}{m} X^T (X \theta - Y)
+$$
+
+### The Gradient Descent algorithm
+
+We can now apply the Gradient Descent algorithm to the matrix form of the cost function and loop until convergence.
+
+We can calculate the new $\theta$ with the following formula:
+$$
+\theta = \theta - \alpha \frac{\partial J}{\partial \theta}
--- trainer.py
+++ trainer.py
@@ -3,30 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# F = X.theta
+"""
+Calculate the model F = X.theta
+Parameters:
+    - x_matrix: matrix of features (input data)
+    - theta_matrix: matrix of parameters (weights of the model)
+"""
 def f_model(x_matrix, theta_matrix):
     return x_matrix.dot(theta_matrix)
 
 
-# Fonction cout J : error measurement (MSE)
+"""
+Calculate the cost function J(theta) = 1/2m * sum((X * theta) - Y)^2
+This function is used to measure the error of the model thanks to the MSE (Mean Squared Error)
+Parameters:
+    - x_matrix: matrix of features
+    - y_matrix: matrix of target values
+    - theta_matrix: matrix of parameters
+"""
 def cost(x_matrix, y_matrix, theta_matrix):
     m = len(y_matrix)
     return 1 / (2 * m) * np.sum((f_model(x_matrix, theta_matrix) - y_matrix) ** 2)
 
 
-# Algo de minimisation : we want to minimize errors thanks to the gradient descent algorithm
-# algo de la descente de gradient
+"""
+Calculate a gradient
+Parameters:
+    - x_matrix: matrix of features
+    - y_matrix: matrix of target values
+    - theta_matrix: matrix of parameters
+"""
 def gradient(x_matrix, y_matrix, theta_matrix):
     m = len(y_matrix)
     return 1 / m * x_matrix.T.dot(f_model(x_matrix, theta_matrix) - y_matrix)
 
 
-def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line, c_line):
+"""
+Gradient descent algorithm
+This is a minimization algorithm that aims to find the best parameters theta that minimize the cost function.
+The algorithm loops until the cost difference between two iterations is negligible.
+Parameters:
+    - x_matrix: matrix of features
+    - y_matrix: matrix of target values
+    - theta_matrix: matrix of parameters
+    - learning_rate: the step between each iteration
+    - pred_line: line on the chart representing the model prediction (F)
+    - c_line: line ont the chart representing the cost evolution
+Returns:
+    - the best theta that minimize the cost function
+"""
+def gradient_descent(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line, c_line):
     cost_history = np.empty((0, 1))
     iter = 0
-    # Gradient descent while minimizing the cost function
     while True:
-        print('iter: ', iter)
         theta_matrix = theta_matrix - learning_rate * gradient(x_matrix, y_matrix, theta_matrix)
         if iter % 100 == 0:
             pred_line.set_ydata(f_model(x_matrix, theta_matrix))
@@ -34,20 +63,30 @@ def descent_gradient(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line,
             cost_history = np.vstack([cost_history, [cost_value]])
             c_line.set_xdata(np.arange(len(cost_history)) * 100)
             c_line.set_ydata(cost_history)
-
             axis[1].relim()
             axis[1].autoscale_view()
-
             plt.pause(0.005)
         # Stop the loop if the cost function converges
         if len(cost_history) > 1 and cost_history[-2] - cost_history[-1] < 0.001:
             break
         iter += 1
-    print("Number of iterations: ", iter)
+    print("🔄 Number of iterations: ", iter)
     return theta_matrix
 
 
-# np.set_printoptions(suppress=True, precision=0)
+"""
+Calculate the performance of the model with the R^2 coefficient
+Parameters:
+    - target: matrix of target values
+    - pred: matrix of predicted values
+"""
+def r_squared(target, pred):
+    u = ((target - pred) ** 2).sum()  # residu de la somme des carrés
+    v = ((y - y.mean()) ** 2).sum()  # somme des carrés totale
+    return 1 - u / v
+
+
+# ------------------- Main -------------------
 file_path = "data.csv"
 
 x = np.empty((0, 1))
@@ -65,37 +104,34 @@ with open(file_path, "r") as file:
 x_copy = x
 y_copy = y
 
-# Normalisation des données
+# Standardization
 x_min = np.min(x)
 x_max = np.max(x)
 x = (x - x_min) / (x_max - x_min)
 
-# So we have m and n and we search for params a and b in the linear model f(x) = ax + b
-# 1st step : F model
-# X matrix
+# X matrix = x with bias
 X = np.hstack((x, np.ones(x.shape)))  # Add a one column for bias
 
-# theta
+# Theta matrix = [theta0, theta1] = [0, 0]
 theta = np.ones((2, 1))
 
 plt.ion()
 figure, axis = plt.subplots(1, 2, figsize=(14, 7))
-
+# First plot (dataset + model)
 axis[0].scatter(x, y)
 prediction_line = axis[0].plot(x, f_model(X, theta), c='r')
 axis[0].set_title("Linear regression")
 axis[0].set_xlabel("Mileage")
 axis[0].set_ylabel("Price")
-
+# Second plot (cost evolution)
 cost_line = axis[1].plot([], [], c='b')
 axis[1].set_title("Cost evolution")
 axis[1].set_xlabel("Number of iterations")
 axis[1].set_ylabel("Cost (MSE)")
-
 plt.tight_layout()
 plt.show()
 
-theta_final = descent_gradient(X, y, theta, 0.01, prediction_line[0], cost_line[0])
+theta_final = gradient_descent(X, y, theta, 0.01, prediction_line[0], cost_line[0])
 
 predictions = f_model(X, theta_final)
 prediction_line[0].set_ydata(predictions)
@@ -106,21 +142,9 @@ file = open(".theta", "w")
 theta_final[1] = theta_final[1] - theta_final[0] * x_min / (x_max - x_min)
 theta_final[0] = theta_final[0] / (x_max - x_min)
 file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
-
 file.close()
 
 plt.ioff()
 plt.show()
 
-
-# Coeficient de determination R^2
-# https://youtu.be/vG6tDQc86Rs?t=1251
-
-
-def r_squared(target, pred):
-    u = ((target - pred) ** 2).sum()  # residu de la somme des carrés
-    v = ((y - y.mean()) ** 2).sum()  # somme des carrés totale
-    return 1 - u / v
-
-
-print("R^2: ", r_squared(y, predictions))
+print("⚡ Performance: ", r_squared(y, predictions) * 100, "%")
