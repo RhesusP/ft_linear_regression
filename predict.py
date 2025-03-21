1b01efc52615081a8e71ab69bee0439f23fde94a
cbernot <camil.bernot.cb@gmail.com>
2025-03-21 09:16:44 +0100

    Clean more readable + fix typos

--- predict.py
+++ predict.py
@@ -1,17 +1,32 @@
-# Load thetas from .theta file
-file = open(".theta", "r")
-a = float(file.readline())  # theta_1
-b = float(file.readline())  # theta_0
-file.close()
+def main():
+    # Load thetas from .theta file
+    try:
+        file = open(".theta", "r")
+    except Exception as e:
+        print("Error: Unable to read thetas (", e, ")")
+        exit(1)
+    a = float(file.readline())  # theta_1
+    b = float(file.readline())  # theta_0
+    file.close()
 
-# Mileage input
-mileage = input("Enter the mileage: ")
-mileage = float(mileage)
-if mileage < 0:
-    print("The mileage cannot be negative")
-    exit()
+    # Mileage input
+    mileage = input("Enter the mileage: ")
+    try:
+        mileage = float(mileage)
+    except Exception as e:
+        print("Error: The mileage must be a number (", e, ")")
+        exit(1)
+    if mileage < 0:
+        print("The mileage cannot be negative")
+        exit(1)
 
-# f(x) = a * x + b
-predicted_price = (a * mileage) + b
+    # f(x) = a * x + b
+    predicted_price = (a * mileage) + b
+    if predicted_price < 0:
+        predicted_price = 0
 
-print(f"The predicted price for {mileage} km is: {predicted_price}")
+    print(f"The predicted price for {mileage} km is: {round(predicted_price, 2)}")
+
+
+if __name__ == "__main__":
+    main()
--- trainer.py
+++ trainer.py
@@ -3,56 +3,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-"""
-Calculate the model F = X.theta
-Parameters:
-    - x_matrix: matrix of features (input data)
-    - theta_matrix: matrix of parameters (weights of the model)
-"""
-def f_model(x_matrix, theta_matrix):
+def f_model(x_matrix: np.ndarray, theta_matrix: np.ndarray) -> np.ndarray:
+    """
+    Calculate the model F = X.theta
+    :param x_matrix: matrix of features (input data)
+    :param theta_matrix: matrix of parameters (weights of the model)
+    :return:
+    """
     return x_matrix.dot(theta_matrix)
 
 
-"""
-Calculate the cost function J(theta) = 1/2m * sum((X * theta) - Y)^2
-This function is used to measure the error of the model thanks to the MSE (Mean Squared Error)
-Parameters:
-    - x_matrix: matrix of features
-    - y_matrix: matrix of target values
-    - theta_matrix: matrix of parameters
-"""
-def cost(x_matrix, y_matrix, theta_matrix):
+def cost(x_matrix: np.ndarray, y_matrix: np.ndarray, theta_matrix: np.ndarray) -> float:
+    """
+    Calculate the cost function J(theta) = 1/2m * sum((X * theta) - Y)^2
+    This function is used to measure the error of the model thanks to the MSE (Mean Squared Error)
+    :param x_matrix: matrix of features
+    :param y_matrix: matrix of target values
+    :param theta_matrix: matrix of parameters
+    :return:
+    """
     m = len(y_matrix)
     return 1 / (2 * m) * np.sum((f_model(x_matrix, theta_matrix) - y_matrix) ** 2)
 
 
-"""
-Calculate a gradient
-Parameters:
-    - x_matrix: matrix of features
-    - y_matrix: matrix of target values
-    - theta_matrix: matrix of parameters
-"""
-def gradient(x_matrix, y_matrix, theta_matrix):
+def gradient(x_matrix: np.ndarray, y_matrix: np.ndarray, theta_matrix: np.ndarray) -> np.ndarray:
+    """
+    Calculate a gradient
+    :param x_matrix: matrix of features
+    :param y_matrix: matrix of target values
+    :param theta_matrix: matrix of parameters
+    :return:
+    """
     m = len(y_matrix)
     return 1 / m * x_matrix.T.dot(f_model(x_matrix, theta_matrix) - y_matrix)
 
 
-"""
-Gradient descent algorithm
-This is a minimization algorithm that aims to find the best parameters theta that minimize the cost function.
-The algorithm loops until the cost difference between two iterations is negligible.
-Parameters:
-    - x_matrix: matrix of features
-    - y_matrix: matrix of target values
-    - theta_matrix: matrix of parameters
-    - learning_rate: the step between each iteration
-    - pred_line: line on the chart representing the model prediction (F)
-    - c_line: line ont the chart representing the cost evolution
-Returns:
-    - the best theta that minimize the cost function
-"""
-def gradient_descent(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line, c_line):
+def gradient_descent(x_matrix: np.ndarray, y_matrix: np.ndarray, theta_matrix: np.ndarray, learning_rate: float,
+                     axis: plt.Axes, pred_line: plt.Line2D, c_line: plt.Line2D) -> np.ndarray:
+    """
+    Gradient descent algorithm
+    This is a minimization algorithm that aims to find the best parameters theta that minimize the cost function.
+    The algorithm loops until the cost difference between two iterations is negligible.
+    :param x_matrix: matrix of features
+    :param y_matrix: matrix of target values
+    :param theta_matrix: matrix of parameters
+    :param learning_rate: the step between each iteration
+    :param axis: the chart axis
+    :param pred_line: line on the chart representing the model prediction (F)
+    :param c_line: line on the chart representing the cost evolution
+    :return: the best theta that minimize the cost function
+    """
     cost_history = np.empty((0, 1))
     iter = 0
     while True:
@@ -67,91 +67,93 @@ def gradient_descent(x_matrix, y_matrix, theta_matrix, learning_rate, pred_line,
             axis[1].autoscale_view()
             plt.pause(0.005)
         # Stop the loop if the cost function converges
-        if len(cost_history) > 1 and cost_history[-2] - cost_history[-1] < 0.001:
+        if len(cost_history) > 1 and cost_history[-2] - cost_history[-1] < 0.01:
             break
         iter += 1
     print("🔄 Number of iterations: ", iter)
     return theta_matrix
 
 
-"""
-Calculate the performance of the model with the R^2 coefficient
-Parameters:
-    - target: matrix of target values
-    - pred: matrix of predicted values
-"""
-def r_squared(target, pred):
+def r_squared(target: np.ndarray, pred: np.ndarray) -> float:
+    """
+    Calculate the performance of the model with the R^2 coefficient
+    :param target: matrix of target values
+    :param pred: matrix of predicted values
+    :return:
+    """
     u = ((target - pred) ** 2).sum()  # residu de la somme des carrés
-    v = ((y - y.mean()) ** 2).sum()  # somme des carrés totale
+    v = ((target - target.mean()) ** 2).sum()  # somme des carrés totale
     return 1 - u / v
 
 
 # ------------------- Main -------------------
-file_path = "data.csv"
+def main():
+    file_path = "data.csv"
+    x = np.empty((0, 1))
+    y = np.empty((0, 1))
 
-x = np.empty((0, 1))
-y = np.empty((0, 1))
+    # Read the dataset
+    try:
+        with open(file_path, "r") as file:
+            csvreader = csv.reader(file)
+            i = 0
+            for idx_row, row in enumerate(csvreader):
+                if idx_row > 0:
+                    x = np.vstack([x, [int(row[0])]])
+                    y = np.vstack([y, [int(row[1])]])
+    except Exception as e:
+        print("Error: Unable to read the dataset (", e, ")")
+        exit(1)
 
-# Read the dataset
-try:
-    with open(file_path, "r") as file:
-        csvreader = csv.reader(file)
-        i = 0
-        for idx_row, row in enumerate(csvreader):
-            if idx_row > 0:
-                x = np.vstack([x, [int(row[0])]])
-                y = np.vstack([y, [int(row[1])]])
-except Exception as e:
-    print("Error: Unable to read the dataset (", e, ")")
-    exit(1)
+    # Normalization
+    x_min = np.min(x)
+    x_max = np.max(x)
+    x = (x - x_min) / (x_max - x_min)
 
-x_copy = x
-y_copy = y
+    # X matrix = x with bias
+    X = np.hstack((x, np.ones(x.shape)))  # Add a one column for bias
 
-# Standardization
-x_min = np.min(x)
-x_max = np.max(x)
-x = (x - x_min) / (x_max - x_min)
+    # Theta matrix = [theta0, theta1] = [0, 0]
+    theta = np.ones((2, 1))
 
-# X matrix = x with bias
-X = np.hstack((x, np.ones(x.shape)))  # Add a one column for bias
+    plt.ion()
+    figure, axis = plt.subplots(1, 2, figsize=(14, 7))
+    # First plot (dataset + model)
+    axis[0].scatter(x, y)
+    prediction_line = axis[0].plot(x, f_model(X, theta), c='r')
+    axis[0].set_title("Linear regression")
+    axis[0].set_xlabel("Mileage")
+    axis[0].set_ylabel("Price")
+    # Second plot (cost evolution)
+    cost_line = axis[1].plot([], [], c='b')
+    axis[1].set_title("Cost evolution")
+    axis[1].set_xlabel("Number of iterations")
+    axis[1].set_ylabel("Cost (MSE)")
+    plt.tight_layout()
+    plt.show()
 
-# Theta matrix = [theta0, theta1] = [0, 0]
-theta = np.ones((2, 1))
+    theta_final = gradient_descent(X, y, theta, 0.01, axis, prediction_line[0], cost_line[0])
 
-plt.ion()
-figure, axis = plt.subplots(1, 2, figsize=(14, 7))
-# First plot (dataset + model)
-axis[0].scatter(x, y)
-prediction_line = axis[0].plot(x, f_model(X, theta), c='r')
-axis[0].set_title("Linear regression")
-axis[0].set_xlabel("Mileage")
-axis[0].set_ylabel("Price")
-# Second plot (cost evolution)
-cost_line = axis[1].plot([], [], c='b')
-axis[1].set_title("Cost evolution")
-axis[1].set_xlabel("Number of iterations")
-axis[1].set_ylabel("Cost (MSE)")
-plt.tight_layout()
-plt.show()
+    predictions = f_model(X, theta_final)
+    prediction_line[0].set_ydata(predictions)
+    plt.pause(0.005)
 
-theta_final = gradient_descent(X, y, theta, 0.01, prediction_line[0], cost_line[0])
+    try:
+        file = open(".theta", "w")
+        # Remove the standardization before saving thetas
+        theta_final[1] = theta_final[1] - theta_final[0] * x_min / (x_max - x_min)
+        theta_final[0] = theta_final[0] / (x_max - x_min)
+        file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
+        file.close()
+    except Exception as e:
+        print("Error: Unable to save thetas (", e, ")")
 
-predictions = f_model(X, theta_final)
-prediction_line[0].set_ydata(predictions)
-plt.pause(0.005)
+    plt.ioff()
+    plt.show()
 
-try:
-    file = open(".theta", "w")
-    # Remove the standardization before saving thetas
-    theta_final[1] = theta_final[1] - theta_final[0] * x_min / (x_max - x_min)
-    theta_final[0] = theta_final[0] / (x_max - x_min)
-    file.write(str(theta_final[0][0]) + "\n" + str(theta_final[1][0]))
-    file.close()
-except Exception as e:
-    print("Error: Unable to save thetas (", e, ")")
+    score = r_squared(y, predictions) * 100
+    print(f"⚡ Performance: {round(score, 2)}%")
 
-plt.ioff()
-plt.show()
 
-print("⚡ Performance: ", r_squared(y, predictions) * 100, "%")
+if __name__ == "__main__":
+    main()
